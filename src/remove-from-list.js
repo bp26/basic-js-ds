b3288d2c994c431fb73272865408f57763282394
bp26 <94074742+bp26@users.noreply.github.com>
2022-08-17 23:12:19 +0300

    feat: remove from list task completed

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -2,120 +2,23 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const { ListNode } = require('../extensions/list-node.js');
 
+function removeKFromList(node, k) {
+  if (node === null) {return null}
+  if (!node.next & node.value === k) {return null}
+  if (!node.next) {return node}
 
-
-
-
-class ListedList {
-  constructor() {
-    this.head = null
-    this.length = null
-  }
-
-  add(value) {
-    if (this.head === null) {
-      this.head = new ListNode(value)
+  if (node.value === k) {
+    let next = node.next
+    if (next.value !== k) {
+      node = node.next
     } else {
-      let current = this.head
-      while (current.next) {
-        current = current.next
-      }
-
-      current.next = new ListNode(value)
-    }
-
-    this.length++
-  }
-
-  insert(position, value) {
-    if (position < 0 || position > this.length) {
-      return false
-    }
-
-    let node = new ListNode(value)
-
-    if (position === 0) {
-      
-      node.next = this.head
-      this.head = node
-    } else {
-      let index = 0
-      let current = this.head
-      let prev = null
-      while (index < position) {
-        prev = current
-        current = current.next
-        index++
-      }
-      prev.next = node
-      node.next = current
-    }
-
-    this.length++
-  }
-
-  get(position) {
-    if (position < 0 || position >= this.length) {
-      return null
-    }
-
-    let current = this.head
-    let index = 0
-    while (index < position) {
-      current = current.next
-      index++
-    }
-    return current.value
-  }
-
-  removeAt(position) {
-    if (position < 0 || position >= this.length) {
-      return false
-    }
-
-    let current = this.head
-
-    if (position === 0) {
-      this.head = current.next
-    } else {
-      let index = 0
-      let prev = null
-      while (index < position) {
-        prev = current
-        current = current.next
-        index++
-      }
-
-      prev.next = current.next
-    }
-    this.length--
-  }
-
-  indexOf(value) {
-    if (this.head === null) {
-      return false
-    }
-
-    let current = this.head
-    let index = 0
-    while (current) {
-      if (current.value === value) {
-        return index
-      }
-
-      current = current.next
-      index++
+      node = removeKFromList(node.next, k)
+      return node
     }
   }
-
-  remove(value) {this.removeAt(this.indexOf(value))}
-}  
-
-
-function removeKFromList(list, k) {
-  console.log(list)
+  node.next = removeKFromList(node.next, k)
+  return node
 }
-
 
 module.exports = {
   removeKFromList
